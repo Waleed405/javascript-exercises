f0c6264ddcb3cfc3abc8422e9b6e3f63428a5d3d
Waleed Hussein <62118101+Waleed405@users.noreply.github.com>
2022-12-13 18:10:32 +0200

    completed calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,37 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a+b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a-b
 };
 
-const sum = function() {
-	
+const sum = function(a) {
+	const thingy = a.reduce((total, c) => {
+    	return add(total, c);
+  	} ,0);
+
+  	return thingy;
 };
 
-const multiply = function() {
-
+const multiply = function(a) {
+	const thingy = a.reduce((product, c) => {
+		return product*c;
+	}, 1)
+	return thingy;
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	return a**b;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+	let total = 1;
+	for(let i = a; i>1; i--) {
+		total *= i;
+	}
+
+	return total;
 };
 
 // Do not edit below this line
