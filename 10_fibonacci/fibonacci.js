3f150d65d3956c89914da67088c10ff48923e04f
Waleed Hussein <62118101+Waleed405@users.noreply.github.com>
2022-12-14 18:22:31 +0200

    completed fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,43 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    if(n<0) {
+        return "OOPS"
+    }
 
+    // return naiveSolution(n);
+    return solution(n);
 };
+
+const naiveSolution = function(n) {
+    if(n==0) {
+        return 0;
+    }
+
+    if(n==1) {
+        return 1;
+    }
+
+    return fibonacci(n-1) + fibonacci(n-2);
+}
+
+const solution = function(n) {
+    let cache = {};
+
+    if(n==0) {
+        return 0;
+    }
+
+    if(n==1) {
+        return 1;
+    }
+
+    if(cache[n] != undefined) {
+        return cache[n];
+    }
+
+    let value = fibonacci(n-1) + fibonacci(n-2);
+    cache[n] = value;
+    return value;
+}
 
 // Do not edit below this line
 module.exports = fibonacci;
