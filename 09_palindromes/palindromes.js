7f01bc4c5b2cac9fad493f04ac7abb4ed69acf9e
Waleed Hussein <62118101+Waleed405@users.noreply.github.com>
2022-12-14 17:40:43 +0200

    completed palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,34 @@
-const palindromes = function () {
+const palindromes = function (paramWord) {
+
+    paramWord = filter(paramWord);
+
+    return evenPalindrome(paramWord);
 
 };
+
+const evenPalindrome = function(localWord) {
+    for(let i = 0; i < localWord.length/2; i++) {
+        if(localWord[i] != localWord[localWord.length-1-i]) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+const filter = function(localWord) {
+    let finalWord = "";
+    Array.from(localWord).forEach(char => {
+        if(isLetter(char)) {
+            finalWord += char.toLowerCase();
+        }
+    });
+    return finalWord;
+}
+
+const isLetter = function (paramChar) {
+    return paramChar.toLowerCase() != paramChar.toUpperCase();
+}
 
 // Do not edit below this line
 module.exports = palindromes;
