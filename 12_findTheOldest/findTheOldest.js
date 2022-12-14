1ddc44039a71d762dd6723aab9eb826cfb6d8d31
Waleed Hussein <62118101+Waleed405@users.noreply.github.com>
2022-12-14 18:55:29 +0200

    completed findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,22 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    let oldest = {};
+    let oldestAge = 0;
+    people.forEach(person => {
+        let age;
+        if(person.yearOfDeath == undefined) {
+            age = (new Date()).getFullYear() - person.yearOfBirth;
+        }
+        else {
+            age = person.yearOfDeath - person.yearOfBirth;
+        }
 
+        if(age > oldestAge) {
+            oldest = person;
+            oldestAge = age;
+        }
+    });
+
+    return oldest;
 };
 
 // Do not edit below this line
